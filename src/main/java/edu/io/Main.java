d45a38b3d20c86a471508c5ffe57b026481a0189
GTKondur <konradkucharski4@gmail.com>
2025-10-19 20:27:25 +0200

    poprawki

--- src/main/java/edu/io/Main.java
+++ src/main/java/edu/io/Main.java
@@ -20,15 +20,26 @@ public class Main {
         board.display();
         Scanner scanner = new Scanner(System.in);
     while (true) {
-
-        String komenda = scanner.nextLine();
-        switch (komenda) {
-            case "w": player.move(PlayerToken.Move.UP);break;
-            case "s": player.move(PlayerToken.Move.DOWN);break;
-            case "a": player.move(PlayerToken.Move.LEFT);break;
-            case "d": player.move(PlayerToken.Move.RIGHT);break;
+        try {
+            String komenda = scanner.nextLine();
+            switch (komenda) {
+                case "w":
+                    player.move(PlayerToken.Move.UP);
+                    break;
+                case "s":
+                    player.move(PlayerToken.Move.DOWN);
+                    break;
+                case "a":
+                    player.move(PlayerToken.Move.LEFT);
+                    break;
+                case "d":
+                    player.move(PlayerToken.Move.RIGHT);
+                    break;
+            }
+            board.display();
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
         }
-        board.display();
     }
 
 
