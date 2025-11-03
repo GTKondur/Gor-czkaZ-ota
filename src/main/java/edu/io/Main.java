4487bf09a2da334eb3bf1bae12a155fdbf331d46
GTKondur <konradkucharski4@gmail.com>
2025-11-03 18:52:06 +0100

    poprawa

--- src/main/java/edu/io/Main.java
+++ src/main/java/edu/io/Main.java
@@ -1,8 +1,16 @@
 package edu.io;
 
+import edu.io.player.Player;
+
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
+        Player player = new Player();
+
+        game.setupBoard();
+        game.join(player);
         game.start();
+
+        game.run();
     }
 }
